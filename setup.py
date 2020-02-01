f134bd1f5856cf117e8d15c50b6634af1f0d0e40
KRedCell <krov_red_cell@outlook.com>
2020-02-01 23:16:21 +0800

    v0.23

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Pointa",
-    version="0.2.2",
+    version="0.2.3",
     author="KRedCell",
     author_email="krov_red_cell@outlook.com",
     description="Pointa Dedicated Server & CLI Based Client",
